66af92314e39cb52ca1d20e4fcd2326807f61c00
OmarAlrifaee <jivara211@gmail.com>
2024-04-23 16:11:01 +0300

    replaced the browser router with hashRouter for gh

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Watch from "./pages/Watch";
